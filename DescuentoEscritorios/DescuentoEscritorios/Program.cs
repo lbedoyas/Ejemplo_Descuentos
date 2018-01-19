194381401ed3afd8405e824ec7e1f9da6367a39c
LB <lucho.bedoya1@hotmail.com>
2018-01-18 19:49:29 -0500

    Ejemplo_Descuentos
    Un almacén de escritorios hace los siguientes descuentos: si el cliente compra menos de 5 unidades se le da un descuento del 10% sobre la compra; si el número de unidades es mayor o igual a cinco pero menos de 10 se le otorga un 20% y, si son 10 o más se le da un 40%.
    Hacer un programa que determine cuánto debe pagar un cliente si el valor de cada escritorio es de $650.000

---
+++ DescuentoEscritorios/DescuentoEscritorios/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescuentoEscritorios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroEscritorios;
+            decimal valorApagar;
+            PedirDatos(out numeroEscritorios);
+            valorApagar = CalcularValor(numeroEscritorios);
+            MostrarResultados(valorApagar);
+        }
+
+        static void PedirDatos(out int numeroEscritorios)
+        {
+            Console.Write("Ingrese numero de escritorios: ...  ");
+            numeroEscritorios = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        static decimal CalcularValor(int numeroEscritorios)
+        {
+            decimal valor = numeroEscritorios * 650000;
+            if (numeroEscritorios < 5) return valor * 0.9M;
+            if (numeroEscritorios < 10) return valor * 0.8M;
+            return valor * 0.6M;
+            
+        }
+
+        static void MostrarResultados(decimal valorApagar)
+        {
+            Console.WriteLine("El valor a pagar es: ${0:N0}",valorApagar);
+            Console.ReadKey();
+        }
+    }
+}
